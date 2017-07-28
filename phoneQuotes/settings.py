17f8fd0136e49e2ace247b4b503f1b6a74a3f483
Adhaar Sharma <asharma327@gmail.com>
2017-07-28 12:28:08 -0400

    took out collection

--- phoneQuotes/settings.py
+++ phoneQuotes/settings.py
@@ -135,9 +135,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 #
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, '/static'),
+# )
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
