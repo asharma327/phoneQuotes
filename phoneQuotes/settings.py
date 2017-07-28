1de2c5f6f59c15940c0a7429aec2d733eeb3d87a
Adhaar Sharma <asharma327@gmail.com>
2017-07-28 11:26:42 -0400

    added snippet to urls.py

--- phoneQuotes/settings.py
+++ phoneQuotes/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 # For Dev
 # STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # For Heroku
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 #
 STATICFILES_DIRS = (
